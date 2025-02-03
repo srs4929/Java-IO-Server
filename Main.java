744e74075f583dca20f51b6116a6a924a19d9b53
srs4929 <149858942+srs4929@users.noreply.github.com>
2025-02-03 12:36:07 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        char c;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+               // Reading the character from keyboard
+        
+        System.out.println("Enter the character and for quit give q")   ;    
+        do
+        {
+            c=(char)br.read(); //read the character
+            System.out.println(c);
+
+        } while(c!='q') ;     
+    }
+}
+
